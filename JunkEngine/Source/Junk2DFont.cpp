b712eda2459d10dbc6b6ca6505d2bbac565bb2e2
G_Garnet <kmu9833@gmail.com>
2016-08-17 23:02:47 +0900

    주석 정리

--- JunkEngine/Source/Junk2DFont.cpp
+++ JunkEngine/Source/Junk2DFont.cpp
@@ -3,21 +3,21 @@
 
 Junk2DFont::Junk2DFont() : Junk2DSprite()
 {
-	file = NULL;                        // font texture
-	graphics = NULL;                    // pointer to graphics
-	color = graphicsNS::WHITE;          // default to white font
-	backColor = TRANSCOLOR;             // default to transparent (no) fill
+	file = NULL;                        // 폰트 텍스쳐
+	graphics = NULL;                    // 그래픽 객체의 포인터
+	color = graphicsNS::WHITE;          // 색
+	backColor = TRANSCOLOR;             // 배경색
 	align = TextStyle::LEFT;
-	width = TextStyle::GRID_WIDTH - 3;     // -2 for transparent border and -1 for divider line
+	width = TextStyle::GRID_WIDTH - 3;     // 
 	height = TextStyle::GRID_HEIGHT - 3;
 	fontHeight = TextStyle::FONT_HEIGHT;
 	spriteData.width = 0;
 	spriteData.height = 0;
-	spriteData.rect.bottom = 1;         // rectangle to select parts of an image
+	spriteData.rect.bottom = 1;         // 
 	spriteData.rect.right = 1;
-	for (int row = 0; row<TextStyle::ROWS; row++)         // for each row of characters in font
+	for (int row = 0; row<TextStyle::ROWS; row++)         // 
 	{
-		for (int col = 0; col<TextStyle::COLUMNS; col++)  // for each col of characters in font
+		for (int col = 0; col<TextStyle::COLUMNS; col++)  // 
 		{
 			fontData[row][col].left = MAXINT;
 			fontData[row][col].right = 0;
@@ -40,7 +40,7 @@ Junk2DFont::~Junk2DFont()
 bool Junk2DFont::initialize(Graphics *g, const char *file)
 {
 	try {
-		graphics = g;           // pointer to graphics object
+		graphics = g;           
 		UINT w, h;
 		HRESULT result = graphics->loadTexture(file, TRANSCOLOR, w, h, textureData);
 		if (FAILED(result))
@@ -49,69 +49,54 @@ bool Junk2DFont::initialize(Graphics *g, const char *file)
 			return false;
 		}
 
-		// textureData.width & textureData.height contain size of entire font texture
-		// Each character has a 1 pixel wide border
-		// There are ROWS * COLS characters
-
-		// Lock the font texture, required to access the pixel data
 		D3DLOCKED_RECT rect;
 		result = textureData->LockRect(0, &rect, NULL, D3DLOCK_READONLY);
-		if (FAILED(result))                          // if lock failed
+		if (FAILED(result))                         
 		{
 			SAFE_RELEASE(textureData);
 			return false;
 		}
 
-		for (DWORD row = 0; row<TextStyle::ROWS; row++)   // for each row of characters in font
+		for (DWORD row = 0; row<TextStyle::ROWS; row++)   
 		{
-			for (DWORD col = 0; col<TextStyle::COLUMNS; col++)    // for each col of characters in font
+			for (DWORD col = 0; col<TextStyle::COLUMNS; col++)    
 			{
-				fontData[row][col].left = MAXINT;    // initialize fontData
+				fontData[row][col].left = MAXINT;    
 				fontData[row][col].right = 0;
 
-				// Process each character pixel by pixel    
-				// for y = top pixel; y <= bottom pixel; y++
 				for (DWORD y = row*TextStyle::GRID_HEIGHT + 1; y<(row + 1)*TextStyle::GRID_HEIGHT - 1; y++)
 				{
-					// Get a pointer to the start of this scanline in the texture
 					DWORD* pBits = (DWORD*)((BYTE*)rect.pBits + y*rect.Pitch);
-					// Process this line of pixels
 					for (DWORD x = col*TextStyle::GRID_WIDTH + 1; x<(col + 1)*TextStyle::GRID_WIDTH - 1; x++)
 					{
-						// Get this pixel
 						DWORD dwPixel = pBits[x];
 
-						// If the alpha is not transparent
 						if ((dwPixel & 0xff000000) != 0x00)
 						{
-							if (x < fontData[row][col].left)     // if this pixel is more left
-								fontData[row][col].left = x;    // save as left edge of character
-							if (x > fontData[row][col].right)    // if this pixel is more right
-								fontData[row][col].right = x;   // save right edge of character
+							if (x < fontData[row][col].left)     
+								fontData[row][col].left = x;   
+							if (x > fontData[row][col].right)    
+								fontData[row][col].right = x;   
 						}
 					}
 				}
 			}
 		}
 
-		// Done with the texture, so unlock it
 		textureData->UnlockRect(0);
 
-		// release this font texture, we just needed it to get font spacing
 		SAFE_RELEASE(textureData);
 
-		// load the font image into a texture manager for use
 		if (!fontTexture.initialize(graphics, file))
-			return false;                   // if error loading font texture
-											// prepare the font image
+			return false;    
 		if (!Junk2DSprite::initialize(graphics, TextStyle::FONT_WIDTH, TextStyle::FONT_HEIGHT, 0, &fontTexture))
-			return false;                   // if failed
+			return false;              
 	}
 	catch (...)
 	{
 		return false;
 	}
-	return true;                    // successful
+	return true;                  
 }
 
 
@@ -130,7 +115,7 @@ void Junk2DFont::print(const std::string &str)
 
 void Junk2DFont::print(const std::string &str, int x, int y, TextStyle::Alignment al)
 {
-	align = al;             // save alignment
+	align = al;            
 	print(str, x, y);
 	align = TextStyle::LEFT;
 }
@@ -151,29 +136,28 @@ void Junk2DFont::print(const std::string &str, int x, int y)
 	for (UINT i = 0; i<str.length(); i++)
 	{
 		ch = str.at(i);
-		if (ch > TextStyle::MIN_CHAR && ch <= TextStyle::MAX_CHAR)    // if displayable character
+		if (ch > TextStyle::MIN_CHAR && ch <= TextStyle::MAX_CHAR)    
 		{
-			chN = ch - TextStyle::MIN_CHAR;                // make min_char index 0
+			chN = ch - TextStyle::MIN_CHAR;              
 			spriteData.rect.top = chN / TextStyle::COLUMNS * TextStyle::GRID_HEIGHT + 1;
 			spriteData.rect.bottom = spriteData.rect.top + TextStyle::FONT_HEIGHT;
 			if (proportional)
 			{
 				spriteData.rect.left = fontData[chN / TextStyle::COLUMNS][chN % TextStyle::COLUMNS].left;
-				// DirectX wants right + 1
 				spriteData.rect.right = fontData[chN / TextStyle::COLUMNS][chN % TextStyle::COLUMNS].right + 1;
 				width = spriteData.rect.right - spriteData.rect.left;
-				if (width >= TextStyle::FONT_WIDTH)         // if full width character do not add spacing
+				if (width >= TextStyle::FONT_WIDTH)       
 				{
-					width = TextStyle::FONT_WIDTH;         // limit width
+					width = TextStyle::FONT_WIDTH;         
 					spriteData.rect.left = chN % TextStyle::COLUMNS * TextStyle::GRID_WIDTH + 1;
 					spriteData.rect.right = spriteData.rect.left + TextStyle::FONT_WIDTH;
 				}
-				else    // not full width so add spacing between characters
+				else    
 					width += proportionalSpacing;
 				scaledWidth = static_cast<int>(width*spriteData.scale);
 				drawChar(ch);
 			}
-			else    // fixed pitch
+			else  
 			{
 				width = TextStyle::FONT_WIDTH;
 				spriteData.rect.left = chN % TextStyle::COLUMNS * TextStyle::GRID_WIDTH + 1;
@@ -182,11 +166,11 @@ void Junk2DFont::print(const std::string &str, int x, int y)
 			}
 			spriteData.x += scaledWidth;
 		}
-		else    // else, non displayable character
+		else  
 		{
 			switch (ch)
 			{
-			case ' ':                            // space
+			case ' ':                         
 				if (proportional)
 				{
 					width = TextStyle::FONT_WIDTH / 2;
@@ -195,22 +179,23 @@ void Junk2DFont::print(const std::string &str, int x, int y)
 				drawChar(' ');
 				spriteData.x += scaledWidth;
 				break;
-				// newline advances 1 line down and sets left edge to starting x screen position,
-				// not left edge of screen
-			case '\n':                            // newline
+
+			case '\n':                            
 				spriteData.x = (float)x;
 				spriteData.y += static_cast<int>(height*spriteData.scale);
 				saveY = spriteData.y;
 				str2 = str.substr(i, str.length());
 				doAlign(str2);
 				spriteData.y = saveY;
+
 				break;
-			case '\r':                            // return to starting x position
+			case '\r':                        
 				spriteData.x = (float)x;
 				str2 = str.substr(i, str.length());
 				doAlign(str2);
 				break;
-			case '\t':                            // tab
+
+			case '\t':                           
 				width = TextStyle::FONT_WIDTH;
 				scaledWidth = static_cast<int>(width*spriteData.scale);
 				tabX = static_cast<int>(spriteData.x) / (scaledWidth * tabSize);
@@ -225,22 +210,22 @@ void Junk2DFont::print(const std::string &str, int x, int y)
 					}
 					else
 					{
-						width = tabW;        // fractional part of character to align with tab stop
+						width = tabW;    
 						drawChar(' ');
 						spriteData.x += tabW;
 					}
 					tabW -= scaledWidth;
 				}
 				break;
-			case '\b':                            // backspace
+			case '\b':                           
 				spriteData.x -= scaledWidth;
 				if (spriteData.x < 0)
 					spriteData.x = 0;
 				break;
-			case '\v':                            // vertical tab
+			case '\v':                           
 				spriteData.y += static_cast<int>(height*spriteData.scale);
 				break;
-			case 0x01:                            // font signature character
+			case 0x01:                            
 				spriteData.rect.top = 1;
 				spriteData.rect.bottom = 1 + TextStyle::FONT_HEIGHT;
 				spriteData.rect.left = 1;
@@ -256,34 +241,34 @@ void Junk2DFont::print(const std::string &str, int x, int y)
 
 void Junk2DFont::doAlign(const std::string &str)
 {
-	if (spriteData.texture == NULL)  // if no texture
+	if (spriteData.texture == NULL)  
 		return;
 
 	UINT w, h;
 	switch (align) {
-	case TextStyle::CENTER:            // center at x and align top to y
+	case TextStyle::CENTER:           
 		getWidthHeight(str, w, h);
 		spriteData.x -= w / 2;
 		break;
-	case TextStyle::RIGHT:             // right justify at x,y
+	case TextStyle::RIGHT:             
 		getWidthHeight(str, w, h);
 		spriteData.x -= w;
 		break;
-	case TextStyle::CENTER_MIDDLE:     // center at x and vertical middle to y
+	case TextStyle::CENTER_MIDDLE:     
 		getWidthHeight(str, w, h);
 		spriteData.x -= w / 2;
 		spriteData.y -= h / 2;
 		break;
-	case TextStyle::CENTER_BOTTOM:     // center at x and align bottom to y
+	case TextStyle::CENTER_BOTTOM:     
 		getWidthHeight(str, w, h);
 		spriteData.x -= w / 2;
 		spriteData.y -= h;
 		break;
-	case TextStyle::LEFT_BOTTOM:       // left justify at x and align bottom to y
+	case TextStyle::LEFT_BOTTOM:      
 		getWidthHeight(str, w, h);
 		spriteData.y -= h;
 		break;
-	case TextStyle::RIGHT_BOTTOM:      // right justify at x and align bottom to y
+	case TextStyle::RIGHT_BOTTOM:   
 		getWidthHeight(str, w, h);
 		spriteData.x -= w;
 		spriteData.y -= h;
@@ -293,7 +278,7 @@ void Junk2DFont::doAlign(const std::string &str)
 
 void Junk2DFont::getWidthHeight(const std::string &str, UINT &w, UINT &h)
 {
-	if (spriteData.texture == NULL)         // if no texture
+	if (spriteData.texture == NULL)  
 		return;
 
 	UCHAR ch = 0, chN = 0;
@@ -306,19 +291,17 @@ void Junk2DFont::getWidthHeight(const std::string &str, UINT &w, UINT &h)
 	for (UINT i = 0; i<str.length(); i++)
 	{
 		ch = str.at(i);
-		// if displayable character
 		if (ch > TextStyle::MIN_CHAR && ch <= TextStyle::MAX_CHAR)
 		{
-			chN = ch - TextStyle::MIN_CHAR;    // make min_char index 0
+			chN = ch - TextStyle::MIN_CHAR;    
 			if (proportional)
 			{
 				spriteData.rect.left = fontData[chN / TextStyle::COLUMNS][chN % TextStyle::COLUMNS].left;
-				// +1 for DirectX sprite width
 				spriteData.rect.right = fontData[chN / TextStyle::COLUMNS][chN % TextStyle::COLUMNS].right + 1;
 				width = spriteData.rect.right - spriteData.rect.left + proportionalSpacing;
 				scaledWidth = static_cast<int>(width*spriteData.scale);
 			}
-			else    // fixed pitch
+			else  
 			{
 				width = TextStyle::FONT_WIDTH;
 				spriteData.rect.left = chN % TextStyle::COLUMNS * TextStyle::GRID_WIDTH + 1;
@@ -326,11 +309,11 @@ void Junk2DFont::getWidthHeight(const std::string &str, UINT &w, UINT &h)
 			}
 			stringWidth += scaledWidth;
 		}
-		else    // else, non displayable character
+		else   
 		{
 			switch (ch)
 			{
-			case ' ':   // space
+			case ' ': 
 				if (proportional)
 				{
 					width = (TextStyle::FONT_WIDTH) / 2;
@@ -338,18 +321,18 @@ void Junk2DFont::getWidthHeight(const std::string &str, UINT &w, UINT &h)
 				}
 				stringWidth += scaledWidth;
 				break;
-			case '\n':  // newline
+			case '\n': 
 				if (strW == 0)
 					strW = stringWidth;
 				stringWidth = 0;
 				h += static_cast<int>(height*spriteData.scale);
 				break;
-			case '\r':  // return
+			case '\r':
 				if (strW == 0)
 					strW = stringWidth;
 				stringWidth = 0;
 				break;
-			case '\t':  // tab
+			case '\t': 
 			{
 				width = TextStyle::FONT_WIDTH;
 				scaledWidth = static_cast<int>(width*spriteData.scale);
@@ -362,7 +345,6 @@ void Junk2DFont::getWidthHeight(const std::string &str, UINT &w, UINT &h)
 						stringWidth += scaledWidth;
 					else
 					{
-						// fractional part of character to align with tab stop
 						width = tabW;
 						stringWidth += tabW;
 					}
@@ -370,12 +352,12 @@ void Junk2DFont::getWidthHeight(const std::string &str, UINT &w, UINT &h)
 				}
 			}
 			break;
-			case '\b':      // backspace
+			case '\b':     
 				stringWidth -= scaledWidth;
 				if (stringWidth < 0)
 					stringWidth = 0;
 				break;
-			case 0x01:      // special
+			case 0x01:     
 				stringWidth += scaledWidth;
 				break;
 			}
@@ -389,20 +371,18 @@ void Junk2DFont::getWidthHeight(const std::string &str, UINT &w, UINT &h)
 
 void Junk2DFont::drawChar(UCHAR ch)
 {
-	SpriteData sd2 = spriteData;    // copy sprite data
+	SpriteData sd2 = spriteData;  
 
-									// display backColor color
-	if (backColor != TRANSCOLOR)     // if backColor is not transparent
+	if (backColor != TRANSCOLOR) 
 	{
 		spriteData.rect.top = (TextStyle::SOLID - TextStyle::MIN_CHAR) / TextStyle::COLUMNS * TextStyle::GRID_HEIGHT + 1;
 		spriteData.rect.bottom = spriteData.rect.top + TextStyle::GRID_HEIGHT - 2;
 		spriteData.rect.left = (TextStyle::SOLID - TextStyle::MIN_CHAR) % TextStyle::COLUMNS * TextStyle::GRID_WIDTH + 1;
 		spriteData.rect.right = spriteData.rect.left + width;
-		draw(backColor);        // draw backColor
-		spriteData.rect = sd2.rect;     // restore character rect
+		draw(backColor);       
+		spriteData.rect = sd2.rect;   
 	}
 
-	// display underline
 	if (underline)
 	{
 		spriteData.rect.top = (TextStyle::UNDERLINE - TextStyle::MIN_CHAR) / TextStyle::COLUMNS * TextStyle::GRID_HEIGHT + 1;
@@ -410,14 +390,13 @@ void Junk2DFont::drawChar(UCHAR ch)
 		spriteData.rect.left = (TextStyle::UNDERLINE - TextStyle::MIN_CHAR) % TextStyle::COLUMNS * TextStyle::GRID_WIDTH + 1;
 		spriteData.rect.right = spriteData.rect.left + width;
 		draw(color);
-		spriteData.rect = sd2.rect;     // restore character rect
+		spriteData.rect = sd2.rect;   
 	}
 
-	// display character
-	if (ch > TextStyle::MIN_CHAR && ch <= TextStyle::MAX_CHAR) // if displayable character
+	if (ch > TextStyle::MIN_CHAR && ch <= TextStyle::MAX_CHAR)
 	{
 		draw(spriteData, color);
-		if (bold)   // bold is done by displaying the character twice with offset x
+		if (bold)  
 		{
 			spriteData.x += TextStyle::BOLD_SIZE*spriteData.scale;
 			draw(spriteData, color);
